7d8d8d074beb9e65c73b1ce136e3e96b9493d057
Adam Suskin <adamsuskin@gmail.com>
2015-10-05 20:50:00 -0400

    minor bug fix

--- reverttab.js
+++ reverttab.js
@@ -1,26 +1,30 @@
 //Author: Adam Suskin
 //License: MIT
 
-var last_id;
-var current_id;
-var tmp_id;
+var last_id = {val: undefined};
+var current_id = {val: undefined};
+var tmp_id = {val: undefined};
+
+function set_id_callback(id_to_update, tab) {
+	id_to_update.val = tab.id;
+}
 
 function set_id_to_current(id_to_update) {
-	chrome.tabs.query({active: true,
-							 lastFocusedWindow: true},
-							 function(tabs) {
-			id_to_update = tabs[0].id;
+	chrome.tabs.query({active: true, lastFocusedWindow: true},
+	function(tabs) {
+		var tab = tabs[0];
+		set_id_callback(id_to_update, tab);
 	});
 }
 
 function update_id() {
-	last_id = current_id;
+	last_id.val = current_id.val;
 	set_id_to_current(current_id);
 }
 
 function revert_tab() {
 	set_id_to_current(tmp_id);
-	if(tmp_id == current_id)
+	if(tmp_id.val == current_id.val)
 		chrome.tabs.update(last_id, {selected: true});
 }
 
