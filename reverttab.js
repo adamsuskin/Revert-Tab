4de3d719e01968171492f1e3275271655d012e27
Adam Suskin <adamsuskin@gmail.com>
2015-10-05 20:16:01 -0400

    Refactoring

--- reverttab.js
+++ reverttab.js
@@ -2,33 +2,25 @@
 //License: MIT
 
 var last_id;
-var this_id;
+var current_id;
 var tmp_id;
 
-function set_id_to_current(tab) {
-	this_id = tab.id;
-}
-
-function check_equal(tab) {
-	return chr
+function set_id_to_current(id_to_update) {
+	chrome.tabs.query({active: true,
+							 lastFocusedWindow: true},
+							 function(tabs) {
+			id_to_update = tabs[0].id;
+	});
 }
 
 function update_id() {
-	last_id = this_id;
-	chrome.tabs.query({active: true,
-							 lastFocusedWindow: true},
-							 function(tabs) {
-			this_id = tabs[0].id;
-	});
+	last_id = current_id;
+	set_id_to_current(current_id);
 }
 
 function revert_tab() {
-	chrome.tabs.query({active: true,
-							 lastFocusedWindow: true},
-							 function(tabs) {
-			tmp_id = tabs[0].id;
-	});
-	if(tmp_id == this_id)
+	set_id_to_current(tmp_id);
+	if(tmp_id == current_id)
 		chrome.tabs.update(last_id, {selected: true});
 }
 
